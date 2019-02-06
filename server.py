003ede55ce3361e7dc175631b26d7ea1de735fc8
Maksym Dakal <dakalm@gmail.com>
2019-02-06 16:29:38 +0200

    added routing and variable

--- server.py
+++ server.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return "hello world!"
 
+@app.route('/blog')
+def blog():
+    return 'This is the blog'
+
+@app.route('/blog/<string:blog_id>')
+def blogpost(blog_id):
+    return f'This is blog post {blog_id}'
+
+# or
+# @app.route('/blog/<blog_id>')
+# def blogpost(blog_id):
+#     return f'This is blog post {str(blog_id)}'
 
 if __name__ == '__main__':
     app.run()
